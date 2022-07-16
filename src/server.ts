b77231b26e312ecff7e88456890ba51e4afef773
Samiur-UTI <samiurkhan94@gmail.com>
2022-07-16 13:28:48 +0600

    project bootstrapped

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import 'module-alias/register';
+import App from './app';
+// import PostController from '@/resources/post/post.controller';
+// import UserController from '@/resources/user/user.controller';
+
+const app = new App(
+    [],
+    Number(process.env.PORT)
+);
+
+app.listen();
